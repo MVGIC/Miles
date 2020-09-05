508544f1d735f13a6f597461f072607e03880a27
MVGIC <ex3ct@mail.ru>
2020-09-05 21:08:28 +0700

    main added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        long TicketPrice = 50000;
+        long MilesBonus = TicketPrice / 20;
+
+        System.out.println(MilesBonus);
+    }
+}
